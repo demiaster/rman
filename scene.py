043142ee86bfd09e9dd2e8b0b0412bbe7d9373b8
demiaster <marta.celeste.feriani@gmail.com>
2017-04-12 20:21:54 +0200

    scene.py: added pointy end

--- scene.py
+++ scene.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
-
+import getpass
+import time
 import prman
+import math
 
 ri = prman.Ri()
 
@@ -19,7 +21,19 @@ ri.WorldBegin()
 
 ri.Translate(0, 0, 2)
 
-ri.Sphere(1, -1, 1, 360)
+# the pointy end
+ri.TransformBegin()
+end_height = 0.3
+ri.Translate(0, end_height, 0)
+ri.Rotate(90, 1, 0, 0)
+ri.Cone(end_height, 0.1, 360)
+ri.TransformEnd()
+
+#the metal thing
+#ri.TransformBegin()
+#ri.Rotate(-90, 1, 0, 0)
+#ri.Cilinder(0.3, 0.1, 360)
+#ri.TransformEnd()
 
 # end of the world
 ri.WorldEnd()
