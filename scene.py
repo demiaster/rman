4f3453c32245f87d5b5d8f04723029c6801adfb8
demiaster <marta.celeste.feriani@gmail.com>
2017-04-20 13:11:32 +0200

    scene.py: lights on, material added

--- scene.py
+++ scene.py
@@ -4,6 +4,7 @@ import time
 import prman
 import math
 
+#funcrion for hyperboloid shapes in the pin
 def hyperboloid_wrapper(height, base_radius, top_radius):
     ri.TransformBegin()
     ri.Rotate(-90, 1, 0, 0)
@@ -12,6 +13,7 @@ def hyperboloid_wrapper(height, base_radius, top_radius):
     ri.Hyperboloid(p_base, p_top, 360)
     ri.TransformEnd()
 
+#the instance for renderman
 ri = prman.Ri()
 
 filename = "scene.rib"
@@ -19,10 +21,9 @@ filename = "scene.rib"
 ri.Begin(filename)
 
 # declare lights
-
-ri.Declare("Light1", "string")
-ri.Declare("Light2", "string")
-ri.Declare("Light3", "string")
+ri.Declare("KeyLight", "string")
+ri.Declare("FillLight", "string")
+ri.Declare("RimLight", "string")
 
 
 # set up the scene
@@ -31,11 +32,20 @@ ri.Format(720, 575, 1)
 ri.Projection(ri.PERSPECTIVE)
 
 # set up the world
-
 ri.WorldBegin()
+
+# turn on the lights
+ri.LightSource("pointlight", {ri.HANDLEID:"KeyLight", "point from": [-35, 125, -10], "float intensity": [1]})
+ri.LightSource("pointlight", {ri.HANDLEID:"FillLight", "point from": [-20, -120, -10], "float intensity": [1]})
+ri.LightSource("pointlight", {ri.HANDLEID:"RimLight", "point from": [-15, -10, -10], "float intensity": [1]})
+
+ri.Illuminate("KeyLight", 1)
+ri.Illuminate("FillLight", 1)
+ri.Illuminate("RimLight", 1)
 
 ri.Translate(0, -1.5, 4)
 
+#model the pin
 # the pointy end
 ri.TransformBegin()
 end_height = 0.35
@@ -59,6 +69,7 @@ ri.Translate(0, metal_height, 0)
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
 disk_radius = 0.45
+ri.Surface("plastic")
 ri.Disk(0, disk_radius, 360)
 ri.TransformEnd()
 
@@ -69,6 +80,7 @@ ri.Translate(0, -0.05, 0)
 ri.Rotate(-90, 1, 0, 0)
 y_max = 0.433
 y_min = 0.05
+ri.Surface("plastic")
 ri.Sphere(bowl_radius, y_min, y_max, 360)
 ri.TransformEnd()
 
@@ -78,6 +90,7 @@ ri.Translate(0, y_max - y_min, 0)
 body_height = 0.7
 body_br = 0.2
 body_tr = 0.15
+ri.Surface("plastic")
 hyperboloid_wrapper(body_height, body_br, body_tr)
 
 # top base (tb)
@@ -85,12 +98,14 @@ ri.TransformBegin()
 ri.Translate(0, body_height, 0)
 tb_height = 0.08
 tb_tr = 0.375
+ri.Surface("plastic")
 hyperboloid_wrapper(tb_height, body_tr, tb_tr)
 
 # top top (tt)
 ri.TransformBegin()
 ri.Translate(0, tb_height, 0)
 tt_tr = 0.35
+ri.Surface("plastic")
 hyperboloid_wrapper(tb_height, tb_tr, tt_tr)
 
 # top cup (tc)
@@ -99,6 +114,7 @@ ri.Translate(0, tb_height, 0)
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
 tc_radius = tt_tr
+ri.Surface("plastic")
 ri.Disk(0, tc_radius, 360)
 ri.TransformEnd()
 
